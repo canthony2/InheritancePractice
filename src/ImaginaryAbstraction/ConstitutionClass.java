8f14c1f9eccf024fca596f0a68302940be8f5cf1
knyghtspup <knyghtspup@10.30.16.140>
2017-02-08 10:45:39 -0600

    Renamed Source Package Folder 10:45 2/8/2017

--- src/ImaginaryAbstraction/ConstitutionClass.java
+++ src/ImaginaryAbstraction/ConstitutionClass.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inheritancepractice.ImaginaryAbstraction;
+package ImaginaryAbstraction;
 
 /**
  *
  * @author knyghtspup
  */
-public class Jaeger {
+public class ConstitutionClass {
     
 }
